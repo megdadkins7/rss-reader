02163b0c693d14d568305d69a7135ea7de96c67d
Meghan Adkins <meghandadkins@gmail.com>
2020-08-12 14:56:33 -0400

    episode component created

---
+++ src/components/Episode.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledEpisode = styled.div`
+  width: 77vw;
+  float: right;
+  margin-right: 1vw;
+`;
+
+const Episode = () => {
+  return (
+    <StyledEpisode>
+      <a href="">title</a>
+    </StyledEpisode>
+  );
+};
+export default Episode;
